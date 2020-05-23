d96003400d827725ba1d6dcd3f7916bb059f5b61
Julissa <a314783@uach.mx>
2020-05-22 22:32:58 -0600

    1

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,19 +1,48 @@
 const express = require('express');
-
+const User = require('../models/user');
 
 function list(req,res,next){
-  res.send('Aqui estaran los usuarios del sistema');
-  //res.render('index', { title: 'mi app de video' });
+  User.find().then(obj => res.status(200).json({
+    message: 'Usuarios del sistema',
+    objs: obj
+  })).catch(err=> res.starus(500).json({
+    message:'Error al caragar los usuarios del sistema',
+    objs:err
+  }));
 }
 
 //regresa un elemento get
 
 function index(req,res,next){
-  res.render('index', { title: 'mi app de video index' });
+  const id = req.params.id;
+  User.findOne({"_id":id}).then(obj => res.status(200).json({
+    message: `Usuario del sistema con id ${id}`,
+    objs: obj
+  })).catch(err=> res.starus(500).json({
+    message:`Error al caragar el usuario del sistema con id=${id}`,
+    objs:err
+  }));
 }
 
 function create(req,res,next){
-  res.render('index', { title: 'mi app de video create ' });
+  let email = req.body.email;
+  let name = req.body.name;
+  let lastName = req.body.lastName;
+  let password = req.body.password;
+
+  let user = new User({
+    _email:email,
+    _name:name,
+    _lastName:lastName,
+    _password:password
+  });
+  user.save().then(obj=>res.status(200).json({
+      message : 'usuario creado correctamente',
+      objs: obj
+    })).catch(err => res.status(500).json({
+      message: 'no se pudo guardar el usuaio',
+      objs: err
+  }));
 }
 
 function update(req,res,next){
@@ -21,7 +50,15 @@ function update(req,res,next){
 }
 
 function destroy(req,res,next){
-  res.render('index', { title: 'mi app de video destroy' });
+  const id = req.params.id;
+  User.remove({"_id":id}).then(obj => res.status(200).json({
+    message: `Usuario del sistema con id ${id} ha sido eliminado`,
+    objs: obj
+  })).catch(err=> res.starus(500).json({
+    message:`Error al eliminar el usuario del sistema con id=${id}`,
+    objs:err
+  }));
+
 }
 
 
